adecad4431e628003db833594b006ffc6b3d9d9e
muntasir.joarder@gmail.com <muntasir.joarder@gmail.com>
2023-08-09 22:48:05 +1000

    New data types

---
+++ src/api/richmedia/controllers/richmedia.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * richmedia controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::richmedia.richmedia');
---
+++ src/api/richmedia/routes/richmedia.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * richmedia router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::richmedia.richmedia');
